9f3b85851e156d66260938cf637c81870b2adb5a
Silvan Büchi <silvan.buechi01@outlook.com>
2023-01-01 22:11:55 +0100

    Added overview

--- HueApiProxy/Controllers/HomeController.cs
+++ HueApiProxy/Controllers/HomeController.cs
@@ -8,25 +8,26 @@ namespace MyStromButton.Controllers
     public class HomeController : ControllerBase
     {
         private readonly IConfiguration configuration;
+        private readonly string hueBridgeIp;
+        private readonly string hueApplicationKey;
 
         public HomeController(IConfiguration configuration)
         {
             this.configuration = configuration;
+            this.hueBridgeIp = this.configuration.GetValue<string>("hue.bridge.ip");
+            this.hueApplicationKey = this.configuration.GetValue<string>("api.hue.application-key");
         }
 
         [HttpGet("toggle/{id}")]
         public async Task<IActionResult> Toggle(string id)
         {
-            string hueBridgeIP = this.configuration.GetValue<string>("hue.bridge.ip");
-            string hueApplicationKey = this.configuration.GetValue<string>("api.hue.application-key");
-
             var handler = new HttpClientHandler();
             handler.ServerCertificateCustomValidationCallback += (a, b, c, d) => true;
 
             var http = new HttpClient(handler);
-            http.DefaultRequestHeaders.Add("hue-application-key", hueApplicationKey);
+            http.DefaultRequestHeaders.Add("hue-application-key", this.hueApplicationKey);
 
-            var lightGetResponse = await http.GetFromJsonAsync<LightGetResponse>($"https://{hueBridgeIP}/clip/v2/resource/light/{id}");
+            var lightGetResponse = await http.GetFromJsonAsync<LightGetResponse>($"https://{this.hueBridgeIp}/clip/v2/resource/light/{id}");
 
             var body = new
             {
@@ -36,15 +37,37 @@ namespace MyStromButton.Controllers
                 }
             };
 
-            await http.PutAsJsonAsync($"https://{hueBridgeIP}/clip/v2/resource/light/{id}", body);
+            await http.PutAsJsonAsync($"https://{this.hueBridgeIp}/clip/v2/resource/light/{id}", body);
 
             return Ok();
         }
 
-        [HttpGet("dashboard")]
-        public async Task<IActionResult> Dashboard()
+        [HttpGet("overview/lights")]
+        public async Task<IActionResult> Lights()
         {
-            return Ok("Test");
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback += (a, b, c, d) => true;
+
+            var http = new HttpClient(handler);
+            http.DefaultRequestHeaders.Add("hue-application-key", this.hueApplicationKey);
+
+            var lightsResponse = await http.GetFromJsonAsync<object>($"https://{this.hueBridgeIp}/clip/v2/resource/light");
+
+            return Ok(lightsResponse);
+        }
+
+        [HttpGet("overview/scenes")]
+        public async Task<IActionResult> Scenes()
+        {
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback += (a, b, c, d) => true;
+
+            var http = new HttpClient(handler);
+            http.DefaultRequestHeaders.Add("hue-application-key", this.hueApplicationKey);
+
+            var scenesResponse = await http.GetFromJsonAsync<object>($"https://{this.hueBridgeIp}/clip/v2/resource/scene");
+
+            return Ok(scenesResponse);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hue-api-proxy
 
-`<ip>:<port>/api/v1/toggle/id/`
+`<ip>:<port>/api/v1/toggle/id`
+`<ip>:<port>/api/v1/overview/lights`
+`<ip>:<port>/api/v1/overview/scenes`
